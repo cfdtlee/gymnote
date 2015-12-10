888619671555b44a2f39bf75cfea7f1eee303073
MathBenDAI <cfdtlee@gmail.com>
2015-12-10 12:06:53 -0500

    gym note

---
+++ gymnote/gymnote/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gymnote
+//
+//  Created by Eric on 11/30/15.
+//  Copyright © 2015 Eric. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
